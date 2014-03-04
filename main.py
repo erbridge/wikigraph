15f2fc487886f8b84eceff67c8d96bea49afdfd6
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2014-03-04 23:01:11 +0000

    Continue the search from the leaves of the previous one.

--- main.py
+++ main.py
@@ -34,42 +34,77 @@ class LinkGetter(object):
     _page_links = []
 
     @classmethod
-    def find_linked_pages(cls, page_name, current_search_depth=0, max_search_depth=2, reset=False):
+    def find_linked_pages(cls, page_name, current_search_depth=0, max_search_depth=2, reset=False, was_leaf=False):
         if reset:
             cls._page_nodes = []
             cls._page_links = []
 
+        leaf = current_search_depth > max_search_depth
+
         page_node = {
-            "name": page_name
+            "name": page_name,
+            "leaf": leaf
         }
 
-        if current_search_depth <= max_search_depth and page_node not in cls._page_nodes:
-            cls._page_nodes.append(page_node)
-            html = UrlGetter.get_html(page_name)
-            if html is not None:
-                linked_pages = set(
-                    re.findall(r"href=(?:\"|\')/wiki/([^:#\"\']+)[^:]*?(?:\"|\')", html))
-                linked_pages -= set(BLACK_LIST + [page_name])
-                for linked_page in linked_pages:
-                    cls._page_links.append({
-                        "source_name": page_name,
-                        "target_name": linked_page
-                    })
-                    cls.find_linked_pages(
-                        linked_page,
-                        current_search_depth=current_search_depth + 1,
-                        max_search_depth=max_search_depth)
+        if was_leaf or page_node not in cls._page_nodes:
+            old_page_node = {
+                "name": page_name,
+                "leaf": was_leaf
+            }
+
+            if old_page_node in cls._page_nodes:
+                cls._page_nodes.remove(old_page_node)
+
+            if page_node not in cls._page_nodes:
+                cls._page_nodes.append(page_node)
+
+            if not leaf:
+                html = UrlGetter.get_html(page_name)
+                if html is not None:
+                    linked_pages = set(
+                        re.findall(r"href=(?:\"|\')/wiki/([^:#\"\']+)[^:]*?(?:\"|\')", html))
+                    linked_pages -= set(BLACK_LIST + [page_name])
+                    for linked_page in linked_pages:
+                        link = {
+                            "source_name": page_name,
+                            "target_name": linked_page
+                        }
+                        if link not in cls._page_links:
+                            cls._page_links.append(link)
+                        cls.find_linked_pages(
+                            linked_page,
+                            current_search_depth=current_search_depth + 1,
+                            max_search_depth=max_search_depth)
 
     @classmethod
-    def get_linked_pages(cls, start_page_name, max_search_depth, file_name=None, reset=False):
+    def get_linked_pages(cls, max_search_depth, start_page_name=None, file_name=None, reset=False, continue_search=True):
         if not reset and file_name and os.path.isfile(file_name):
             with open(file_name, "r") as in_file:
                 data = json.load(in_file)
             cls._page_nodes = data.get("nodes")
             cls._page_links = data.get("links")
 
-        cls.find_linked_pages(start_page_name, current_search_depth=0,
-                              max_search_depth=max_search_depth, reset=reset)
+        if reset:
+            cls._page_nodes = []
+            cls._page_links = []
+
+        if continue_search:
+            for node in cls._page_nodes:
+                if node.get("leaf"):
+                    cls.find_linked_pages(
+                        node.get("name"),
+                        current_search_depth=0,
+                        max_search_depth=max_search_depth,
+                        reset=False,
+                        was_leaf=True)
+
+        if start_page_name:
+            cls.find_linked_pages(
+                start_page_name,
+                current_search_depth=0,
+                max_search_depth=max_search_depth,
+                reset=False,
+                was_leaf=False)
 
         if file_name:
             data = {
@@ -83,4 +118,6 @@ class LinkGetter(object):
 
 
 if __name__ == "__main__":
-    LinkGetter.get_linked_pages("Python", 1, file_name="data/data.json")
+    LinkGetter.get_linked_pages(
+        1, start_page_name="Python", file_name="data/data.json")
+
